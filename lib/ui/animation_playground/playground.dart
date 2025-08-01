01dc27e9ac39b19e2c37ec110aee0074fc925313
Nguyễn Văn Kiên <nguyenvankien@Admins-Mac-mini.local>
2025-08-01 17:27:00 +0700

    feat: WaveContainer

--- lib/ui/animation_playground/playground.dart
+++ lib/ui/animation_playground/playground.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Playground extends StatelessWidget {
@@ -5,65 +7,68 @@ class Playground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: AnimationExample());
+    final screenSize = MediaQuery.sizeOf(context);
+    return MaterialApp(home: WaveContainer(screenSize: screenSize));
   }
 }
 
-class AnimationExample extends StatefulWidget {
-  const AnimationExample({super.key});
+class WaveContainer extends StatefulWidget {
+  const WaveContainer({super.key, required this.screenSize});
+
+  final Size screenSize;
 
   @override
-  State<AnimationExample> createState() => _AnimationExampleState();
+  State<WaveContainer> createState() => _WaveContainerState();
 }
 
-class _AnimationExampleState extends State<AnimationExample>
+class _WaveContainerState extends State<WaveContainer>
     with SingleTickerProviderStateMixin {
-  late final AnimationController controller;
-  Animation<double> opacityAnimation = const AlwaysStoppedAnimation(0.0);
-  final duration = Duration(milliseconds: 500);
+  late final AnimationController _controller;
+  late final Animation<double> _left;
 
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(vsync: this, duration: duration);
-    opacityAnimation = Tween<double>(begin: 0, end: 1).animate(controller);
+    final screenSize = widget.screenSize;
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 5),
+    )..repeat();
+    _left = Tween<double>(
+      begin: -50,
+      end: screenSize.width,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
   }
 
   @override
   void dispose() {
-    controller.dispose();
+    _controller.dispose();
     super.dispose();
-  }
-
-  void togglePlayAction() {
-    if (controller.status == AnimationStatus.completed) {
-      controller.reverse();
-    } else if (controller.status == AnimationStatus.dismissed) {
-      controller.forward();
-    }
   }
 
   @override
   Widget build(BuildContext context) {
+    double baseY = widget.screenSize.height / 2 - 100;
+    double amplitude = 50;
     return Scaffold(
       appBar: AppBar(title: Text("Animation Playground")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          togglePlayAction();
-        },
-        child: Icon(Icons.play_arrow),
-      ),
-      body: Center(
-        child: AnimatedBuilder(
-          animation: opacityAnimation,
-          builder: (context, Widget? child) {
-            return Opacity(
-              opacity: opacityAnimation.value,
-              child: child,
-            );
-          },
-          child: Text("A Thing", style: TextStyle(fontSize: 24)),
-        ),
+      body: Stack(
+        children: [
+          AnimatedBuilder(
+            animation: _controller,
+            builder: (BuildContext context, Widget? child) {
+              double radians = _controller.value * 10 * pi;
+              double sinY = baseY + sin(radians) * amplitude;
+              return Positioned(
+                left: _left.value,
+                top: sinY,
+                child: child!,
+              );
+            },
+            child: Container(color: Colors.blue, width: 50, height: 50),
+          ),
+        ],
       ),
     );
   }
